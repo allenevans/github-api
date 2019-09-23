d16edcd94eae16a586b712f51b51c937adea3faa
Allen Evans <allen@techinity.com>
2019-09-23 07:16:13 +0100

    feat: api wrapper implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import * as core from '@actions/core';
+import GitHub from 'github-api';
 
-(function run() {
+(async function run() {
   try {
-    core.debug('todo');
+    Object.keys(process.env)
+      .filter((key) => /^INPUT_/.test(key))
+      .forEach((key) => {
+        core.debug(`${key}=${process.env[key]}`);
+        console.log(`${key}=${process.env[key]}`);
+      });
+
+    const gh = new GitHub({
+      token: process.env.GITHUB_TOKEN || process.env.GH_TOKEN,
+    });
+
+    const response = await gh.getRepo('allenevans', 'github-api');
+
+    console.log('response', response);
   } catch (error) {
     core.setFailed(error.message);
     process.exit(1);
