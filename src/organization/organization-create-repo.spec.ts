005c18d4c4319f2f87abb132bc46538829908369
Allen Evans <allen@techinity.com>
2019-10-05 11:15:45 +0100

    feat: create organisation repo

---
+++ src/organization/organization-create-repo.spec.ts
@@ -0,0 +1,99 @@
+import GitHub from 'github-api';
+import { mockConfigLoader } from '../utils/mock-config-loader';
+import { classMapping } from '../class-mapping';
+
+const mockGitHub: any = {
+  createRepo: jest.fn(),
+
+  getOrganization: (organization: string) =>
+    Promise.resolve({
+      createRepo: mockGitHub.createRepo,
+    }),
+};
+
+describe('Organization.createRepo', () => {
+  beforeEach(() => {
+    mockGitHub.createRepo.mockImplementation(() =>
+      Promise.resolve({
+        data: {},
+        headers: {},
+        status: 0,
+      }),
+    );
+
+    jest.spyOn(mockGitHub, 'getOrganization');
+  });
+
+  const mockArgs: any[] = [
+    {
+      name: 'test-repo',
+      description: 'Repo for testing github api',
+      homepage: 'https://github.com',
+      private: false,
+      has_issues: true,
+      has_projects: true,
+      has_wiki: true,
+    },
+  ];
+
+  describe('getOrganization', () => {
+    it('should have createRepo method', () => {
+      const api = new GitHub().getOrganization('my-org');
+
+      expect(api.createRepo).toBeDefined();
+    });
+  });
+
+  describe('json', () => {
+    test('Organization.createRepo', async () => {
+      const input = mockConfigLoader(`
+        with:
+          json: |
+            {
+              "command": "Organization.createRepo",
+              "organization": "my-org",
+              "args": [
+                {
+                  "name": "test-repo",
+                  "description": "Repo for testing github api",
+                  "homepage": "https://github.com",
+                  "private": false,
+                  "has_issues": true,
+                  "has_projects": true,
+                  "has_wiki": true
+                }
+              ]
+            }
+      `);
+
+      await classMapping[input.command.apiClass](mockGitHub)(input);
+
+      expect(mockGitHub.getOrganization).toHaveBeenCalledWith('my-org');
+      expect(mockGitHub.createRepo).toHaveBeenCalledWith(...mockArgs);
+    });
+  });
+
+  describe('yaml', () => {
+    test('Organization.createRepo', async () => {
+      const input = mockConfigLoader(`
+        with:
+          yaml: |
+            command: Organization.createRepo
+            organization: my-org
+            args:
+              - name: 'test-repo'
+                description: 'Repo for testing github api'
+                homepage: 'https://github.com'
+                private: false
+                has_issues: true
+                has_projects: true
+                has_wiki: true
+      `);
+
+      await classMapping[input.command.apiClass](mockGitHub)(input);
+
+      expect(mockGitHub.getOrganization).toHaveBeenCalledWith('my-org');
+      expect(mockGitHub.createRepo).toHaveBeenCalledWith(...mockArgs);
+    });
+  });
+});
